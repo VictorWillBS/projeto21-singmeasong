e0f94839b1f70f792c1925e449a3fb6b3ad7a1a8
Victor Barros <victorwbarros@gmail.com>
2022-09-25 03:52:39 -0300

    Feature Create Service Test: Tests created Insert Function / Upvote Function/ Downvote Function

--- back-end/tests/unit/serviceTests.test.ts
+++ back-end/tests/unit/serviceTests.test.ts
@@ -2,7 +2,7 @@ import { recommendationService } from "../../src/services/recommendationsService
 import { recommendationRepository } from "../../src/repositories/recommendationRepository.js";
 import recommendationFactory from "../factory/recomendationFactory";
 describe("Test Insert Function", () => {
-  it("Verify sending correct insert", async () => {
+  it("Insert New Recommendation, Correct Data Format.", async () => {
     jest
       .spyOn(recommendationRepository, "findByName")
       .mockImplementationOnce((): any => {
@@ -21,5 +21,109 @@ describe("Test Insert Function", () => {
 
     expect(recommendationRepository.findByName).toBeCalled();
     expect(recommendationRepository.create).toBeCalled();
+  });
+
+  it("Insert Recommendation Already existent.", async () => {
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockImplementationOnce((): any => {
+        return true;
+      });
+
+    jest
+      .spyOn(recommendationRepository, "create")
+      .mockImplementationOnce((): any => {
+        return;
+      });
+
+    const createData = recommendationFactory.allowedRecomendation();
+
+    const result = recommendationService.insert(createData);
+    expect(result).rejects.toStrictEqual({
+      type: "conflict",
+      message: "Recommendations names must be unique",
+    });
+    expect(recommendationRepository.findByName).toBeCalled();
+  });
+});
+
+describe("Test Upvote Function", () => {
+  it("Try Upvote Correct Id, expect function toBeCalled", async () => {
+    const recommendation = {
+      ...recommendationFactory.allowedRecomendation(),
+      id: 1,
+      score: 10,
+    };
+
+    jest
+      .spyOn(recommendationRepository, "find")
+      .mockResolvedValueOnce(recommendation);
+
+    jest
+      .spyOn(recommendationRepository, "updateScore")
+      .mockResolvedValueOnce(null);
+
+    await recommendationService.upvote(recommendation.id);
+
+    expect(recommendationRepository.find).toBeCalled();
+    expect(recommendationRepository.updateScore).toBeCalled();
+  });
+  it("Try Upvote a Nonexistent Id", () => {
+    jest
+      .spyOn(recommendationRepository, "find")
+      .mockResolvedValueOnce(undefined);
+
+    const result = recommendationService.upvote(1);
+
+    expect(result).rejects.toStrictEqual({
+      type: "not_found",
+      message: "",
+    });
+    expect(recommendationRepository.find).toBeCalled();
+  });
+});
+
+describe("test Downvote Function", () => {
+  it("Test Downvote Correct Id, expect function toBeCalled", async () => {
+    const recommendation = {
+      ...recommendationFactory.allowedRecomendation(),
+      id: 1,
+      score: 15,
+    };
+    jest
+      .spyOn(recommendationRepository, "find")
+      .mockResolvedValueOnce(recommendation);
+    jest
+      .spyOn(recommendationRepository, "updateScore")
+      .mockResolvedValueOnce({ ...recommendation, score: 14 });
+
+    await recommendationService.downvote(1);
+
+    expect(recommendationRepository.find).toBeCalled();
+    expect(recommendationRepository.updateScore).toBeCalled();
+  });
+  it("Test Downvote Recommendation score < -5, expect to try exclude Recommendation", async () => {
+    const recommendation = {
+      ...recommendationFactory.allowedRecomendation(),
+      id: 1,
+      score: -5,
+    };
+    jest
+      .spyOn(recommendationRepository, "find")
+      .mockResolvedValueOnce(recommendation);
+    jest
+      .spyOn(recommendationRepository, "updateScore")
+      .mockResolvedValueOnce({ ...recommendation, score: -6 });
+    jest
+      .spyOn(recommendationRepository, "remove")
+      .mockImplementationOnce((): any => {
+        return;
+      });
+
+    await recommendationService.downvote(recommendation.id);
+
+    expect(recommendationRepository.find).toBeCalled();
+    expect(recommendationRepository.updateScore).toBeCalled();
+    expect(recommendationRepository.remove).toBeCalled();
   });
 });
